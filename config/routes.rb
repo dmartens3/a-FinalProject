ef1ebd20795b7859eef3e58740ca5432167136fc
dmartens3 <dmartens3@rrc.ca>
2024-04-10 10:00:52 -0500

    $ rails g devise:controllers user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
+
   devise_for :admins
   get 'pages/home'
   get 'pages/users'
